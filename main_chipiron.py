91aec0a56c5f9cf3b7f4f334ca521745ca1c9500
victor <victorgabillon@gmail.com>
2022-02-19 06:29:26 +0100

    correcting typos

--- main_chipiron.py
+++ main_chipiron.py
@@ -2,11 +2,12 @@ import sys
 import subprocess
 
 if __name__ == "__main__":
-
     # checking if the version of python is high enough
-    assert sys.version_info >= (3, 9), "A version of Python higher than 3.9 is required to run chipiron"
+    message = 'A version of Python higher than 3.9 is required to run chipiron\n.' + \
+              ' Try using "python3 main_chipiron.py" instead'
 
+    assert sys.version_info >= (3, 9), message
     # launching the real main python script.
     # this allows the to bypass the automatic full interpreter check of python that would raise a syntax error before
     # the assertion above in case of a wrong python version
-    subprocess.Popen(('python3', 'src/main_chipiron_.py') + tuple(sys.argv[1:]))
+    subprocess.Popen(('python3', 'main_chipiron_.py') + tuple(sys.argv[1:]))
