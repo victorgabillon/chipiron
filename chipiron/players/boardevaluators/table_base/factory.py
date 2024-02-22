f8e09ec893403f6efbfd3ef728fb43978a24e0fe
Victor <victor.gabillon@thalesgroup.com>
2024-02-22 09:44:29 +0100

    update dockerfile and making syzygy more optional

--- chipiron/players/boardevaluators/table_base/factory.py
+++ chipiron/players/boardevaluators/table_base/factory.py
@@ -1,9 +1,17 @@
 from chipiron.players.boardevaluators.table_base.syzygy import SyzygyTable
-import multiprocessing
-import queue
+from chipiron.utils import path
+import os
 
-def create_syzygy_thread() -> SyzygyTable:
-    syzygy_table : SyzygyTable = SyzygyTable('')
-   # syzygy_mailbox = multiprocessing.Manager().Queue()
-    #syzygy_table_thread = SyzygyTableThread(syzygy_table, syzygy_mailbox)
+
+def create_syzygy() -> SyzygyTable | None:
+    syzygy_table: SyzygyTable | None
+    path_to_table: path = "data/syzygy-tables/"
+    is_exist: bool = os.path.exists(path_to_table)
+
+    if is_exist:
+        syzygy_table: SyzygyTable = SyzygyTable(path_to_table=path_to_table)
+    else:
+        print('WARNING: no folder found for syzygy tables so NOT using it')
+        syzygy_table = None
+
     return syzygy_table
