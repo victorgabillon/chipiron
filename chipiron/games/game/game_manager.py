a8927bdc3c55b37e31ea89327b48bc1947c814b8
Victor <victor.gabillon@thalesgroup.com>
2024-02-23 14:08:47 +0100

    dockerfile permission

--- chipiron/games/game/game_manager.py
+++ chipiron/games/game/game_manager.py
@@ -21,12 +21,12 @@ from chipiron.utils import path
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-file_handler = logging.FileHandler('sample.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
+#file_handler = logging.FileHandler('sample.log')
+#file_handler.setLevel(logging.DEBUG)
+#file_handler.setFormatter(formatter)
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+#logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
 
