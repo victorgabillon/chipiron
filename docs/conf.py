811a67a5d3a37a0f06bd5e287238b971ac23aea6
Victor <victor.gabillon@thalesgroup.com>
2024-03-27 16:47:24 +0100

    add sphinx docs folder

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Victor Gabillon'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.todo","sphinx.ext.autodoc","sphinx.ext.viewcode"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx-rtd-theme'
 html_static_path = ['_static']
