78ee060cc45d30ce640a51e6fbd793250e33e15e
Victor <victor.gabillon@thalesgroup.com>
2024-03-27 17:41:40 +0100

    add sphinx docs folder

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,11 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+import os
+
+# Import the chess module.
+sys.path.insert(0, os.path.abspath(".."))
 
 project = 'chipiron'
 copyright = '2024, Victor Gabillon'
