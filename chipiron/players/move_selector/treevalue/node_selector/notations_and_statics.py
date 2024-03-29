abbd7935ca6c15d92c7024eddff070644fc1be49
Victor <victor.gabillon@thalesgroup.com>
2024-03-29 15:10:22 +0100

    mypy strict

--- chipiron/players/move_selector/treevalue/node_selector/notations_and_statics.py
+++ chipiron/players/move_selector/treevalue/node_selector/notations_and_statics.py
@@ -30,23 +30,16 @@ def zipf_picks(
         log_term: float = (math.log(math.e * shifted_rank)) ** 2
         weight: float = value * shifted_rank * log_term + .0001
         weights.append(weight)
-        #  print('value', weight, value, shifted_rank, log_term)
-        #  print('wegith', best_weight, weight, best_weight, best_rank)
 
         if best_weight is None or weight < best_weight:
             best_weight = weight
             best_rank = rank
-    # print('bestrank', best_rank)
 
     if random_pick:
         raise Exception('nodt coded yet')  # not codeed properly yet
         # choices = random_generator.choices(list(ranks.keys()), weights=weights, k=1)
-    # print('bestrankzz')
 
-    # return choices[0]
     else:
-        # print('bestrank', weights)
-
         return best_rank
 
 
@@ -57,6 +50,9 @@ def zipf_picks_random(
     length_list = len(ordered_list_elements)
     assert (length_list > 0)
     weights = [1 / (index + 1) / (math.log(math.e * (index + 1))) ** 0 for index in range(length_list)]
-    # print('44',weights, ordered_list_elements)
-    picked_element = random_generator.choices(ordered_list_elements, weights=weights, k=1)
+    picked_element = random_generator.choices(
+        ordered_list_elements,
+        weights=weights,
+        k=1
+    )
     return picked_element[0]
