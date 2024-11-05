6452cd03f9ee00bb94e71ce199d31f5b9ef7a19a
victorgabillon <victorgabillon@gmail.com>
2024-11-05 20:46:29 +0000

    testing for rust versus python divergence

--- tests/integration_test/one_match/test_integration.py
+++ tests/integration_test/one_match/test_integration.py
@@ -8,7 +8,6 @@ from chipiron.scripts.script_args import BaseScriptArgs
 
 # we need to not use multiprocessing to be able to use pytest therefore use setting_cubo  and not setting_jime
 
-# Todo add the check of rust with or without borad mofification
 # todo check that the game is the same with or without rust
 # todo check that the game is the same with or without board mofificaiton
 # todo have a game that starts with an endgame to force to check the syzygy
@@ -285,6 +284,57 @@ def test_randomness():
     print(f'time: {end_time - start_time}')
 
 
+def test_same_game_with_or_without_rust():
+
+
+    import chipiron as ch
+    from chipiron.games.match.match_factories import create_match_manager_from_args
+    from chipiron.games.match.match_results import MatchReport
+    from chipiron.games.match.match_args import MatchArgs
+
+    match_args: MatchArgs = MatchArgs()
+    match_args.seed = 0
+    match_args.file_name_player_one = 'players_for_test_purposes/Uniform.yaml'
+    match_args.file_name_player_two = 'players_for_test_purposes/Sequool.yaml'
+    match_args.file_name_player_two = 'Random.yaml'
+    match_args.file_name_match_setting = 'setting_tron.yaml'
+
+    implementation_args: ImplementationArgs = ImplementationArgs(use_rust_boards=False)
+    base_script_args: BaseScriptArgs = BaseScriptArgs()
+    base_script_args.experiment_output_folder = None
+
+    match_manager: ch.game.MatchManager = create_match_manager_from_args(
+        match_args=match_args,
+        implementation_args=implementation_args,
+        base_script_args=base_script_args
+    )
+    match_report_base: MatchReport = match_manager.play_one_match()
+
+    test_passed: bool = True
+    number_test: int = 1
+
+    for ind in range(number_test):
+        implementation_args: ImplementationArgs = ImplementationArgs(use_rust_boards=True)
+
+        match_manager: ch.game.MatchManager = create_match_manager_from_args(
+            match_args=match_args,
+            implementation_args=implementation_args,
+            base_script_args=base_script_args
+        )
+        match_report: MatchReport = match_manager.play_one_match()
+
+        # checking if two matches launched with the same fixed seed returns the same game moves.
+        print('match_report.match_move_history, match_report_base.match_move_history',
+              match_report.match_move_history, match_report_base.match_move_history)
+        assert (match_report.match_move_history == match_report_base.match_move_history)
+
+    print(match_report)
+
+    end_time = time.time()
+    print(f'time: {end_time - start_time}')
+
+
+
 if __name__ == "__main__":
 
     try:
@@ -298,7 +348,9 @@ if __name__ == "__main__":
     except Exception:
         test_configs = configs_base
 
-    test_one_matches(configs=test_configs)
+    #test_one_matches(configs=test_configs)
 
+    # todo maybe merege the test that verifiy if two game are unchanged when variying some parameters like ranfom and rust atm
+    test_same_game_with_or_without_rust()
     test_randomness()
     print('ALL OK for ONE MATCH')
