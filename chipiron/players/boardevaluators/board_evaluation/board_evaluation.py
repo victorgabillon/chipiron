86610acdae4e4a6bbb84287294b4c623873b7b2f
Victor <victor.gabillon@thalesgroup.com>
2024-12-05 18:06:32 +0100

    u

--- chipiron/players/boardevaluators/board_evaluation/board_evaluation.py
+++ chipiron/players/boardevaluators/board_evaluation/board_evaluation.py
@@ -5,7 +5,6 @@ Module for the BoardEvaluation class and the PointOfView enumeration.
 from dataclasses import dataclass
 from enum import Enum
 
-from chipiron.environments.chess.move import moveUci
 from chipiron.players.boardevaluators.over_event import OverEvent
 from chipiron.environments.chess.move.imove import moveKey
 
