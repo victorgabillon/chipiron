63e564aabc36d6d121d77fe25ab1c13ed10a199c
Victor <victor.gabillon@thalesgroup.com>
2025-03-25 18:15:04 +0100

    remove stockfish

--- MakefileForTest.mk
+++ MakefileForTest.mk
@@ -11,7 +11,7 @@ DATA_SOURCE="https://drive.google.com/drive/folders/1tvkuiaN-oXC7UAjUw-6cIl1PB0r
 DATA_DESTINATION=${ROOT_DIR}/data/
 
 .PHONY: init
-init:  chipiron/stockfish chipiron/data chipiron/requirements
+init:  chipiron/data chipiron/requirements
 
 chipiron/requirements:
 	pip install --no-cache-dir -r  requirements.txt
