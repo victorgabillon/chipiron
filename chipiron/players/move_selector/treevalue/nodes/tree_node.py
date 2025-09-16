ea191a735fc02006d792a61cfa590cde54580a64
Victor <victor.gabillon@thalesgroup.com>
2025-09-16 11:44:33 +0200

    update TreeNode without Any

--- chipiron/players/move_selector/treevalue/nodes/tree_node.py
+++ chipiron/players/move_selector/treevalue/nodes/tree_node.py
@@ -16,7 +16,7 @@ from .itree_node import ITreeNode
 
 
 @dataclass(slots=True)
-class TreeNode[ChildrenType: ITreeNode]:
+class TreeNode[ChildrenType: ITreeNode = ITreeNode]:
     r"""
     The TreeNode class stores information about a specific board position, including the board representation,
     the player to move, the half-move count, and the parent-child relationships with other nodes.
