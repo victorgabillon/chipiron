75841a3ed3b17a21194eaf71a756144511c3b394
victorgabillon <victorgabillon@gmail.com>
2026-01-26 15:49:59 +0100

    Add terminal oracle for syzygy over events

--- chipiron/players/wirings/chess_wiring.py
+++ chipiron/players/wirings/chess_wiring.py
@@ -7,16 +7,21 @@ from chipiron.players.boardevaluators.table_base.factory import (
     AnySyzygyTable,
     create_syzygy_factory,
 )
+from chipiron.players.adapters.chess_syzygy_oracle import (
+    ChessSyzygyPolicyOracle,
+    ChessSyzygyTerminalOracle,
+    ChessSyzygyValueOracle,
+)
 from chipiron.players.factory import create_game_player
 from chipiron.players.observer_wiring import ObserverWiring
 
 if TYPE_CHECKING:
-    from atomheart.board import IBoard
     from atomheart.board.utils import FenPlusHistory
     from valanga import Color
 
     from chipiron.players.game_player import GamePlayer
     from chipiron.players.player_args import PlayerFactoryArgs
+    from chipiron.environments.chess.types import ChessState
     from chipiron.scripts.chipiron_args import ImplementationArgs
     from chipiron.utils.dataclass import IsDataclass
     from chipiron.utils.queue_protocols import PutQueue
@@ -34,22 +39,31 @@ class BuildChessGamePlayerArgs:
 def build_chess_game_player(
     args: BuildChessGamePlayerArgs,
     queue_out: PutQueue[IsDataclass],
-) -> GamePlayer[FenPlusHistory, IBoard]:
+) -> GamePlayer[FenPlusHistory, ChessState]:
     create_syzygy = create_syzygy_factory(
         use_rust=args.implementation_args.use_rust_boards
     )
     syzygy = args.syzygy_table if args.syzygy_table is not None else create_syzygy()
+    policy_oracle = (
+        ChessSyzygyPolicyOracle(syzygy) if syzygy is not None else None
+    )
+    value_oracle = ChessSyzygyValueOracle(syzygy) if syzygy is not None else None
+    terminal_oracle = ChessSyzygyTerminalOracle(syzygy) if syzygy is not None else None
     return create_game_player(
         player_factory_args=args.player_factory_args,
         player_color=args.player_color,
-        syzygy_table=syzygy,
+        policy_oracle=policy_oracle,
+        value_oracle=value_oracle,
+        terminal_oracle=terminal_oracle,
         queue_progress_player=queue_out,
         implementation_args=args.implementation_args,
         universal_behavior=args.universal_behavior,
     )
 
 
-CHESS_WIRING: ObserverWiring[FenPlusHistory, IBoard, BuildChessGamePlayerArgs] = (
+CHESS_WIRING: ObserverWiring[
+    FenPlusHistory, ChessState, BuildChessGamePlayerArgs
+] = (
     ObserverWiring(
         build_game_player=build_chess_game_player,
         build_args_type=BuildChessGamePlayerArgs,
