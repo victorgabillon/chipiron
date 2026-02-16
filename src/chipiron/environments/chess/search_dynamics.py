d1b325600da5bbfb6ea773733443bfe1b84eaa72
victorgabillon <victorgabillon@gmail.com>
2026-02-16 18:49:34 +0000

    bad bunny
    Signed-off-by: victorgabillon <victorgabillon@gmail.com>

--- src/chipiron/environments/chess/search_dynamics.py
+++ src/chipiron/environments/chess/search_dynamics.py
@@ -14,6 +14,8 @@ from chipiron.environments.chess.types import ChessState
 class ChessSearchDynamics(SearchDynamics[ChessState]):
     """Search dynamics with depth-aware board-copy policy."""
 
+    __anemone_search_dynamics__ = True
+
     copy_stack_until_depth: int = 2
     deep_copy_legal_moves: bool = True
 
