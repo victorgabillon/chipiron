110d2cfd7640f3a68044c29e5b7c23cd97c8cfe1
victorgabillon <victorgabillon@gmail.com>
2026-02-22 09:13:06 +0000

    lighter test bqsetreeseqrch

--- tests/integration_test/base_tree_exploration/test_base_tree_exploration.py
+++ tests/integration_test/base_tree_exploration/test_base_tree_exploration.py
@@ -1,7 +1,18 @@
 """Module for test base tree exploration."""
 
+from parsley import make_partial_dataclass_with_optional_paths
+
+from chipiron.players.player_ids import PlayerConfigTag
 import chipiron.scripts as scripts
+from chipiron.scripts.base_tree_exploration.base_tree_exploration import (
+    BaseTreeExplorationArgs,
+)
 from chipiron.scripts.factory import create_script
+
+
+PartialOpMatchScriptArgs = make_partial_dataclass_with_optional_paths(
+    cls=BaseTreeExplorationArgs
+)
 
 
 def test_base_tree_exploration() -> None:
@@ -9,6 +20,9 @@ def test_base_tree_exploration() -> None:
     print("Running the SCRIPT with config ")
     script_object: scripts.IScript = create_script(
         script_type=scripts.ScriptType.BASE_TREE_EXPLORATION,
+        extra_args=PartialOpMatchScriptArgs(
+            player_config_tag=PlayerConfigTag.UNIFORM_DEPTH,
+        ),
         should_parse_command_line_arguments=False,
     )
 
